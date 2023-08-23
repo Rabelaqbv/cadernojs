b3ef4a22fa9b341f5bc11c028f8505a1c03435ef
isa <125407240+Rabelaqbv@users.noreply.github.com>
2023-08-23 15:01:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function data(){
+    document.getElementById('demo').innerHTML = Date();
+}
+
+function ola(){
+    document.getElementById('demi').innerHTML = "Nao perca tempo com bobagens...perca com coisas que lhe diverte!";
+}
